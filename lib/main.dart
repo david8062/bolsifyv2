454041aa27de67dbc4287ea05388de6431006112
jdavid806 <jeffersondavila1101@gmail.com>
2025-11-09 20:58:13 -0500

    initial proyect

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
-import 'styles/themes/app_theme_light.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:bolsifyv2/styles/themes/app_theme_light.dart';
+import 'package:bolsifyv2/src/router/app_router.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+
+    return MaterialApp.router(
       title: 'Bolsify',
       theme: AppThemeLight.theme,
-      home: const Scaffold(
-        body: Center(child: Text('Bolsify MVP 🚀')),
-      ),
+      debugShowCheckedModeBanner: false,
+      routerConfig: router,
     );
   }
 }
--- lib/src/router/app_router.dart
+++ lib/src/router/app_router.dart
@@ -1,8 +1,8 @@
-import 'package:bolsifyv2/src/features/splash/view/splash_view.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:flutter/material.dart';
-
+import 'package:bolsifyv2/src/features/splash/view/splash_view.dart';
+import 'package:bolsifyv2/src/features/home/view/home_view.dart';
 
 final routerProvider = Provider<GoRouter>((ref) {
   return GoRouter(
@@ -10,8 +10,13 @@ final routerProvider = Provider<GoRouter>((ref) {
     routes: [
       GoRoute(
         path: '/',
-        name: 'home',
+        name: 'splash',
         builder: (context, state) => const SplashView(),
+      ),
+      GoRoute(
+        path: '/home',
+        name: 'home',
+        builder: (context, state) => const HomeView(),
       ),
     ],
   );
